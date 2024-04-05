12d3cae148f1e4c7b69dc5202798310a427d5af0
Eduardo Perucci <peruccii2917@hotmail.com>
2024-04-05 17:47:39 -0300

    handleError function

---
+++ src/app/errors/handleErrors.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React from "react";
+import { ErrorPage } from "./error-page";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | undefined;
+}
+
+export default class StandardErrorBoundary extends React.Component<{}, ErrorBoundaryState> {
+  constructor( public props: any) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+      error: undefined
+    };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return {
+      hasError: true,
+      error: error
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log("Error caught!");
+    console.error(error);
+    console.error(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorPage />;
+    } else {
+      return this.props.children;
+    }
+  }
+}
