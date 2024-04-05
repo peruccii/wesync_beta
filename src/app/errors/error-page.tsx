d5d297399419a27af5e47a51edc811f2749507ac
Eduardo Perucci <peruccii2917@hotmail.com>
2024-04-05 17:47:06 -0300

    page error boundary 404

---
+++ src/app/errors/error-page.tsx
@@ -0,0 +1,20 @@
+export const ErrorPage = (props: any) => {
+ return (
+    <>
+    <div className="w-full h-[53rem] flex justify-center items-center">
+       <div className="text-center flex flex-col justify-center h-full  w-full items-center">
+    <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
+    <p className="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
+    <div className="animate-bounce">
+      <svg className="mx-auto h-16 w-16 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+      </svg>
+    </div>
+    <p className="mt-4 text-gray-600">Let's get you back <a href="/" className="text-blue-500">Home</a>.</p>
+  </div>
+    </div>
+  
+
+    </>
+ );
+}
