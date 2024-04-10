92d614015cd8199b94007c162e1e14ed5b53bee0
Eduardo Perucci <peruccii2917@hotmail.com>
2024-04-10 16:19:27 -0300

    initial_page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,15 @@ import ContextPageThird from "@/components/initial_page/ctxpage_3"
 import ContextPageFour from "@/components/initial_page/ctxpage_4"
 import Header from "@/components/initial_page/header"
 
-import StandardErrorBoundary from "./errors/handleErrors"
-
-
 const HomePage = () => {
   return (
-    <>
-    
-      <div className="pt-[4.75rem] bg-[#040202] lg:pt-[5.25rem] overflow-hidden ">
+      <div className="pt-[4.75rem] bg-color-n-principal lg:pt-[5.25rem] overflow-hidden ">
         <Header />
         <ContextPageOne />
         <ContextPageSecond />
         <ContextPageThird />
         <ContextPageFour />
       </div>
-  
-    </>
   )
 };
 
